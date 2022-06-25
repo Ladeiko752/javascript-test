20753da76a5d82481acec4f38b84f2295900090b
Anton <ladeikolkd@mail.ru>
2022-06-25 18:57:27 +0300

    work with inputs

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function substraction(){
         document.querySelector('.current-val').innerHTML = --currentValue;
     }
 }
+
+function getValue(inputClass){
+    return document.querySelector(inputClass).value;
+}
+
+document.querySelector(".button-text").onclick = () => {
+    let value = getValue(".input-text");
+    console.log(value);
+}
+
+document.querySelector(".button-range").onclick = () => {
+    let value = getValue(".range-input");
+    console.log(value);
+}
+
+document.querySelector(".checkbox-button").onclick = () => {
+    let condition = document.querySelector(".checkbox-input");
+    console.log(condition.checked);
+}
+
