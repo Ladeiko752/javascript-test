b451ca6d2f668933172aba8ea9d97b9e1ec41dc0
Anton <ladeikolkd@mail.ru>
2022-06-26 20:14:27 +0300

    added task with bracket and event

--- script.js
+++ script.js
@@ -33,3 +33,34 @@ document.querySelector(".checkbox-button").onclick = () => {
     console.log(condition.checked);
 }
 
+document.querySelector(".textarea-button").onclick = (event) => {
+    event.preventDefault(); 
+    let text = document.querySelector(".myform");
+    console.log(text.elements.mytext.value);
+    console.log(text.elements.myinput.value);
+}
+
+document.querySelector(".task-button").onclick = () => {
+    let openBracket = 0;
+    let closeBracket = 0;
+    let storage = document.querySelector(".task-input").value;
+    let array = storage.split('');
+    console.log(array);
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] == "("){
+            ++openBracket;
+        }else{
+            ++closeBracket;
+        }
+    }
+
+    console.log(`Quantity open bracket: ${openBracket}`)
+    console.log(`Quantity open bracket: ${closeBracket}`)
+
+    if(openBracket != closeBracket){
+        document.querySelector(".answer-div").innerHTML = "False";
+    }else{
+        document.querySelector(".answer-div").innerHTML = "True";
+    }
+}
